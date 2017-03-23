421152ec3ce679a0b5dd28c47793dcb404f243fc
Dayne Broderson <broderson@gmail.com>
2017-03-22 16:40:35 -0800

    update versions

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['vagrant']['version'] = '1.9.2'
-default['terraform']['version'] = '0.8.6'
+default['vagrant']['version'] = '1.9.3'
+default['terraform']['version'] = '0.9.1'
 
 default['chef_dk']['version'] = '1.2.22-1'
